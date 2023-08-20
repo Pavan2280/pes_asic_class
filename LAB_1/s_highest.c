135797cd36f4628cfe0a4b18ea4cc82eaca92aac
Pavan S Talegaon <131603225+Pavan2280@users.noreply.github.com>
2023-08-20 11:10:16 -0400

    Delete LAB_1 directory

--- LAB_1/s_highest.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-
-int main() {
-
-	long long int max = (long long int) (pow(2,10) * -1); 
-	printf("Highest number represented by long long int is %lld\n", max);
-	return 0;
-}
